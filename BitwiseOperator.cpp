3705eaff3ae3a93ea31f6673f2122e65f6baa741
WILSONCHINYAMA <167409018+WILSONCHINYAMA@users.noreply.github.com>
2024-06-01 00:27:29 +0200

    Add files via upload

---
+++ BitwiseOperator.cpp
@@ -0,0 +1,23 @@
+# include<iostream>
+using namespace std;
+main(){
+    unsigned int a=60;
+    unsigned int b=13;
+    int c = 0;
+
+    c = a & b;
+    cout <<"Line 1 - Value of c is: "<< c <<endl;
+
+    c = a | b;
+    cout <<"Line 2 - Value of c is: "<< c <<endl;
+
+    c = a ^ b;
+    cout <<"Line 3 - Value of c is: "<< c <<endl;
+    c = ~a;
+    cout << "Line 4 - Value of c is: "<< c <<endl;
+    c = a << 2;
+    cout << "Line 5 - Value of c is: "<< c <<endl;
+    c = a >> 2;
+    cout << "Line 6 - Value of c is: "<< c << endl;
+    return 0;
+}
---
+++ GlobalVariables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//Global variables declartion:
+int g;
+int main(){
+    // Local variables declaration:
+    int a,b;
+    // actual initialization
+    a = 10;
+    b = 20;
+    g = a + b;
+
+    cout<<g;
+    return 0;
+}
